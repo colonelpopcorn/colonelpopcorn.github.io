f7ac6fa6f87ee4cec5c6c26dc513000707158712
Jonathan Ling <jonathanling@shamrocksweb.com>
2018-08-21 23:09:16 -0400

    It's the folder name with the dots Maurice...

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <navigation-menu></navigation-menu>
+    <v-toolbar app></v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <div>Hello hot moms.</div>
+
+      </v-container>
+    </v-content>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import NavigationMenu from "./navigation/NavigationMenu.vue";
+export default Vue.extend({
+  data: function() {
+    return {
+      testVal: "Hello World!"
+    };
+  },
+  components: {
+    "navigation-menu": NavigationMenu
+  }
+});
+</script>
+
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+import App from './App.vue';
+
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
+
+console.log("SFDASGASRHEDTH!");
