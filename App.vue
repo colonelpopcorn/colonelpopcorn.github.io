3bc8fec08a0520e3b38c3adcbccb4d097c503291
Jonathan Ling <jonathanling@shamrocksweb.com>
2019-04-25 17:52:06 -0400

    Add stuff and things.

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="center">
+      <h1 class="type-text">This should be red.<span class="cursor-blink">|</span></h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4 col-md-6 col-lg-6">
+      <figure class="profile-image">
+        <img class="round" src="assets/profile.jpg" alt="">
+        <figcaption>Jonathan Ling</figcaption>
+      </figure>
+      <div class="center col-sm-12 col-md-6 col-lg-6">
+        {{ splashContent }}
+      </div>
+    </div>
+      <div class="vertical-rule col-sm-1"></div>
+
+    <div class="col-sm-4 col-md-6 col-lg-6">
+
+      <a href="#">Blog</a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      splashContent: `
+        Hello! Welcome to my website! Here you'll find my personal blog, my tech blog, my github page,
+        my youtube channel, and recruitment flowchart! I hope you enjoy your stay and look forward to hearing from you!
+      `,
+    };
+  },
+});
+</script>
+
--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
-import './app.scss';
-import 'mini.css';
+import Vue from "vue";
+import App from "./App";
+import "./app.scss";
+
+const app = new Vue({
+  el: "#app",
+  render: (h) => h(App),
+});
