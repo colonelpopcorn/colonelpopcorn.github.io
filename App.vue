490998a4baf6b7534fdfdffa24b02e3b21787f15
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2019-04-26 15:06:57 -0400

    Finish basics for portfolio.

--- App.vue
+++ App.vue
@@ -1,12 +1,22 @@
 <template>
-<div class="container">
+<div class="container" style="padding:100px;">
   <div class="row">
     <div class="center">
-      <h1 class="type-text">This should be red.<span class="cursor-blink">|</span></h1>
+      <h1 class="type-text">Get more <vue-typer
+      :text='this.skillsArray'
+      :repeat='Infinity'
+      :shuffle='true'
+      initial-action="erasing"
+      :type-delay='70'
+      :pre-erase-delay='2000'
+      :erase-delay='900'
+      erase-style='select-all'
+      :erase-on-complete='false'
+      caret-animation='smooth'></vue-typer> done.</h1>
     </div>
   </div>
   <div class="row">
-    <div class="col-sm-4 col-md-6 col-lg-6">
+    <div class="col-sm-12 col-md">
       <figure class="profile-image">
         <img class="round" src="assets/profile.jpg" alt="">
         <figcaption>Jonathan Ling</figcaption>
@@ -15,11 +25,14 @@
         {{ splashContent }}
       </div>
     </div>
-      <div class="vertical-rule col-sm-1"></div>
-
-    <div class="col-sm-4 col-md-6 col-lg-6">
-
-      <a href="#">Blog</a>
+    <div class="col-sm-12 col-md">
+      <div class="center col-sm-12 col-md-6 col-lg-6">
+        <span class="link-span"><a class="ext-link" href="#">Personal Blog</a></span>
+        <span class="link-span"><a class="ext-link" href="#">Tech Blog</a></span>
+        <span class="link-span"><a class="ext-link" href="https://shamrocksweb.com">Web Design</a></span>
+        <span class="link-span"><a class="ext-link" href="https://small-biz-hosting.com">Web Hosting</a></span>
+        <span class="link-span"><a class="ext-link" href="#">Recruiters</a></span>
+      </div>
     </div>
   </div>
 </div>
@@ -30,10 +43,19 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
+      skillsArray: [
+        "business software",
+        "hardware testing",
+        "cloud services",
+        "serverless apps",
+        "single-page apps",
+        "engineer training",
+      ],
+      // tslint:disable
       splashContent: `
-        Hello! Welcome to my website! Here you'll find my personal blog, my tech blog, my github page,
-        my youtube channel, and recruitment flowchart! I hope you enjoy your stay and look forward to hearing from you!
+        Hello! Welcome to my website! Here you'll find my personal blog, my tech blog, my github page, my youtube channel, and recruitment flowchart! I hope you enjoy your stay and look forward to hearing from you!
       `,
+      // tslint:enable
     };
   },
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import VueTyperPlugin from "vue-typer";
 import App from "./App";
 import "./app.scss";
+
+Vue.use(VueTyperPlugin);
 
 const app = new Vue({
   el: "#app",
